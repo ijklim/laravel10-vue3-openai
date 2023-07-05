9072a35f81245abfc5351807b7d21fe3f1e0a03c
ivan-aim <77643196+ivan-aim@users.noreply.github.com>
2023-07-05 10:18:34 -0400

    Change site title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel 10, Vite 4, Vue 3, Vue Router 4</title>
+        <title>AI Toolbox</title>
 
         <!-- === Fonts === -->
         <link rel="preconnect" href="https://fonts.bunny.net">
